789dc420fae8a4f1cdf4e1f16bb0b0ac48c5f4a4
AbylayNurtayev <krutyev6@gmail.com>
2024-12-22 19:24:08 +0500

    assignmenr finished

--- blockchain.py
+++ blockchain.py
@@ -41,3 +41,39 @@ class Block:
             if block_hash.startswith('0000'):  # Condition for valid hash
                 return block_hash
             nonce += 1
+
+class Blockchain: 
+    def init(self): 
+        self.chain = [] 
+        self.create_genesis_block() 
+ 
+    def create_genesis_block(self): 
+        genesis_block = Block("0", ["Genesis Block"]) 
+        self.chain.append(genesis_block) 
+ 
+    def add_block(self, transactions): 
+        previous_hash = self.chain[-1].hash 
+        new_block = Block(previous_hash, transactions) 
+        self.chain.append(new_block)
+
+
+def validate_blockchain(blockchain): 
+    for i in range(1, len(blockchain.chain)): 
+        current = blockchain.chain[i] 
+        previous = blockchain.chain[i - 1] 
+ 
+        if current.previous_hash != previous.hash: 
+            return False 
+ 
+        if current.merkle_root != current.compute_merkle_root(): 
+            return False 
+ 
+        if not current.hash.startswith('0000'): 
+            return False 
+ 
+    return True 
+ 
+def create_transaction(sender, receiver, amount): 
+    return f"{sender} -> {receiver}: {amount}"
+
+
